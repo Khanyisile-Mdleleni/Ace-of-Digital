f268cb72448631e40d1de362c8c0f5b1338321e1
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-02-06 14:06:39 +0200

    styled the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import logo from "../assets/img/logoo.png"
 
 const Header = () => {
   return (
     <div>
         <nav className='header'>
+        <div className="header__logo">
+          <img
+            src={logo}
+            alt="logo image"
+            style={{
+              width: "45px",
+              height: "45px",
+              marginTop: "20px",
+            }}
+          />
+          <h4
+            style={{
+              position: "absolute",
+              top: "8px",
+              left: "60px",
+              marginTop: "15px",
+            }}
+          >
+            Ace of Digital
+          </h4>
+        </div>
             <ul className='list-items'>
                 <li className='list-item'><a href="#">Home</a></li>
                 <li className='list-item'><a href="#">About Us</a></li>
