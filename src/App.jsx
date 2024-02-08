a18b272d0a676834d21581bd048b431cbfecec9f
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-02-08 11:41:12 +0200

    Created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
-import Header from './components/Header'
+import "./App.css";
+import Header from "./components/Header";
+import {Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";  
+import Services from "./pages/Services";
+import Testimonials from "./pages/Testimonials";
+import Contact from "./pages/Contact";
+
 
 function App() {
   return (
     <>
-      <Header/>
+       <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+       
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import logo from "../assets/img/logoo.png"
+import React from "react";
+import logo from "../assets/img/logoo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-        <nav className='header'>
+      <nav className="header">
         <div className="header__logo">
           <img
             src={logo}
@@ -26,16 +27,27 @@ const Header = () => {
             Ace of Digital
           </h4>
         </div>
-            <ul className='list-items'>
-                <li className='list-item'><a href="#">Home</a></li>
-                <li className='list-item'><a href="#">About Us</a></li>
-                <li className='list-item'><a href="#">Services</a></li>
-                <li className='list-item'><a href="#">Testimonials</a></li>
-                <li className='list-item'><a href="#">Contact Us</a></li>
-            </ul>
-        </nav>
+        <ul className="list-items">
+          <li className="list-item">
+            {" "}
+            <Link to="/">Home</Link>
+          </li>
+          <li className="list-item">
+          <Link to="/about">About Us</Link>
+          </li>
+          <li className="list-item">
+          <Link to="/services">Services</Link>
+          </li>
+          <li className="list-item">
+          <Link to="/testimonials">Testimonials</Link>
+          </li>
+          <li className="list-item">
+          <Link to="/contact">Contact Us</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
