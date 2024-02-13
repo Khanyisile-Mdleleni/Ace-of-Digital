3a7d285966318562c5748fff840d7d98b61dd1fe
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-02-13 15:04:14 +0200

    Added about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import Card from "react-bootstrap/Card";
 
-const About = () => {
+function About() {
   return (
-    <div>About</div>
-  )
+    <>
+      <div>
+        <h1 className="title">About Us</h1>
+      </div>
+      <div className="cards">
+        <div className="card">
+          <Card border="primary" style={{ width: "18rem" }}>
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Primary Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="card">
+          <Card border="secondary" style={{ width: "18rem" }}>
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Secondary Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="card">
+          <Card border="success" style={{ width: "18rem" }}>
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Success Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default About
+export default About;
