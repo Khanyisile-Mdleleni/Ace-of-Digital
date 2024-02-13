63e3d2f5c6767b4ea102937cd9441bad2fa7154c
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-02-13 15:37:52 +0200

    Added content on the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <div>
+        <h1 className="title">Contact Us</h1>
+        <h3 className="subtitle-c">Get in touch</h3>
+      </div>
+      <div>
+        <Container>
+          <Row>
+            <Col>1 of 2</Col>
+            <Col>2 of 2</Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
