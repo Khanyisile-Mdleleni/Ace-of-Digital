46f4507b688e069c236a677fbfa76abf46578b5e
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-02-14 14:06:29 +0200

    Added services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
+    <div>
+      <h1 className='title'>Our Services</h1>
+      <h3 className='subtitle-c'>What we offer</h3>
+
+      <div className="service">
+        <h2>Service 1</h2>
+        <p>Description of Service 1.</p>
+      </div>
+
+      <div className="service">
+        <h2>Service 2</h2>
+        <p>Description of Service 2.</p>
+      </div>
+
+      <div className="service">
+        <h2>Service 3</h2>
+        <p>Description of Service 3.</p>
+      </div>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
