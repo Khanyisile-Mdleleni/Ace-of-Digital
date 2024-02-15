5acad784ccaaf3e39e08fc6ab82b2b7d2495e49c
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-02-15 11:38:36 +0200

    Added testimonials

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/img/back.jpg";
 
 const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+  const testimonials = [
+    {
+      id: 1,
+      image: "url_to_image1.jpg",
+      stars: 5,
+      comment:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      id: 2,
+      image: "url_to_image2.jpg",
+      stars: 4,
+      comment:
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      id: 1,
+      image: "url_to_image1.jpg",
+      stars: 5,
+      comment:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      id: 2,
+      image: "url_to_image2.jpg",
+      stars: 4,
+      comment:
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+  ];
 
-export default Testimonials
+  return (
+    <>
+      <h1 className="title-t">Client Testimonials</h1>
+      <br />
+      <br />
+      <div className="testimonials-container">
+        <div className="testimonials">
+          {testimonials.map((testimonial) => (
+            <div className="testimonial" key={testimonial.id}>
+              <div className="testimonial-image">
+                <img src={testimonial.image} alt="Client" />
+              </div>
+              <div className="testimonial-details">
+                <div className="stars">
+                  {[...Array(testimonial.stars)].map((_, index) => (
+                    <span key={index} className="star">
+                      &#9733;
+                    </span>
+                  ))}
+                </div>
+                <p className="comment">{testimonial.comment}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Testimonials;
